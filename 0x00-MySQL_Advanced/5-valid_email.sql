815377d53fd2fa8a8a3ff13461bc80007f300746
kinoansah <kinoansah@gmail.com>
2024-01-18 11:37:26 +0000

    Email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,4 +1,4 @@
-QL script that creates a trigger that resets 
+-- SQL script that creates a trigger that resets 
 -- the attribute valid_email only when the email has been changed.
 DELIMITER $$
 CREATE TRIGGER reset 
